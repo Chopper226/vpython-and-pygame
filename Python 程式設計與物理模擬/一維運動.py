8f0421c81262f7898fe1a88dc51afd7cd1490ffe
Chopper226 <layu.chen@gmail.com>
2023-02-03 16:38:56 +0800

    一維運動

--- Python 程式設計與物理模擬/一維運動.py
+++ Python 程式設計與物理模擬/一維運動.py
@@ -1,28 +1,42 @@
 from vpython import*
-size=0.5
 
-x=arrow(pos=vector(0,0,0), axis=vector(1,0,0),shaftwidth=0.02,color=color.black)
-ball=sphere(pos=vector(0,0,0), radius=size, color=color.magenta, v=vector(2,0,0),a=vector(-0.5,0,0))
-pic_1=graph(title='x vs t',width=300,height=300,xtitle='t',ytitle='x',
-foreground=color.black,background=color.white,xmax=5,xmin=0,ymax=5,ymin=0, align='left')
-x_t=gcurve(color=color.blue)
-pic_2=graph(title='v vs t',width=300,height=300,xtitle='t',ytitle='x',
-foreground=color.black,background=color.white,xmax=5,xmin=0,ymax=5,ymin=0, align='left')
-v_t=gcurve(color=color.red)
-pic_3=graph(title='a vs t',width=300,height=300,xtitle='t',ytitle='x',
-foreground=color.black,background=color.white,xmax=5,xmin=0,ymax=3,ymin=-2, align='left')
-a_t=gcurve(color=color.green)
+''' Constant '''
+size=0.5
 dt=0.001
 t=0
+
+''' 建立物件 '''
+ball=sphere(pos=vector(0,0,0),radius=size,color=color.magenta,v=vector(4,0,0),a=vector(-2,0,0))
+
+''' xt 圖 '''
+pic_xt=graph(title='x vs t',width=300,height=300,xtitle='t',ytitle='x',foreground=color.black,background=color.white,xmax=5,xmin=0,ymax=5,ymin=-5,align='left')
+
+x_t=gcurve(color=color.blue)
+
+''' vt 圖 '''
+pic_vt=graph(title='v vs t',width=300,height=300,xtitle='t',ytitle='v',foreground=color.black,background=color.white,xmax=5,xmin=0,ymax=6,ymin=-6,align='left')
+
+v_t=gcurve(color=color.red)
+
+''' at 圖 '''
+pic_at=graph(title='a vs t',width=300,height=300,xtitle='t',ytitle='a',foreground=color.black,background=color.white,xmax=5,xmin=0,ymax=4,ymin=-4,align='left')
+
+a_t=gcurve(color=color.green)
+
+
 while t<=5:
     rate(1000)
+        
+    ''' 更改球的位置與速度'''
     ball.pos=ball.pos+ball.v*dt
     ball.v+=ball.a*dt
-    t=t+dt
+    
+    t=t+dt # 經過時間
+    
+    ''' 畫圖'''
     x_t.plot(pos=(t,ball.pos.x))
     v_t.plot(pos=(t,ball.v.x))
     a_t.plot(pos=(t,ball.a.x))
-
 
 
 
